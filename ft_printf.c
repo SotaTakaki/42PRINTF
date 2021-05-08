22a5ae5e68fbfec4147359315350aa94b67f71eb
Sota Takaki <stakaki@c1r1s6.42tokyo.jp>
2021-05-08 22:49:20 +0900

    for vscode

--- ft_printf.c
+++ ft_printf.c
@@ -6,8 +6,27 @@
 /*   By: stakaki <stakaki@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 21:52:36 by stakaki           #+#    #+#             */
-/*   Updated: 2021/05/08 21:53:10 by stakaki          ###   ########.fr       */
+/*   Updated: 2021/05/08 22:48:47 by stakaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf()
+#include <stdarg.h>
+#include <stdio.h>
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	char	d;
+
+	va_start(ap, format);
+	d = (char)va_arg(ap, int);
+	printf("%c\n", d);
+	va_end(ap);
+	return (0);
+}
+
+int	main(void)
+{
+	ft_printf("", 's');
+	return (0);
+}
